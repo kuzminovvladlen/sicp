bc2882b7d40b3fae64315e77cca7f2a3194b5c31
Vladlen Kuzminov <ooyshit@gmail.com>
2018-09-22 22:10:26 +0300

    Add unittest and Makefile

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 SICP on Racket
 ====
 
+### Requirements
+
+* Racket
+* Rackunit (`raco pkg install rackunit`)
+* Make
+
+### Using
+
+1. Clone repository
+1. Run tests `make test`
+
 ### Содержание
 
 - [Глава 1](./doc/index.md#Глава-1-Построение-абстракций-с-помощью-процедур)
--- src/chapter01/1_03.rkt
+++ src/chapter01/1_03.rkt
@@ -1,4 +1,7 @@
-#lang racket
+#lang racket/base
+
+(require rackunit)
+
 (define (square x) (* x x))
 
 (define (sum-of-square a b) (+ (square a) (square b)))
@@ -7,3 +10,12 @@
     (cond ((and (>= a c) (>= b c)) (sum-of-square a b))
           ((and (>= a b) (>= c b)) (sum-of-square a c))
           ((and (>= b a) (>= c a)) (sum-of-square b c))))
+
+
+(check-equal? (square 5) 25)
+(check-not-equal? (square 6) 26)
+(check-equal? (sum-of-square 3 4) 25)
+(check-not-equal? (sum-of-square 3 3) 20)
+(check-equal? (sum-of-square-max-two 3 4 2) 25)
+(check-equal? (sum-of-square-max-two 3 4 5) 41)
+(check-equal? (sum-of-square-max-two 5 4 6) 61)
